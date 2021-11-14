ff998dc2bc4614c8add6eba3cba7259d3e69aa7f
NorbertRuff <ruffnorbert88@gmail.com>
2021-11-15 00:25:16 +0100

    [add] app config

---
+++ config/app-config.js
@@ -0,0 +1,8 @@
+export const appConfig = { port: process.env.PORT };
+
+export const API_BASE_URL = "https://simple-rest-api.onrender.com";
+
+export const POWER_ROUTES = {
+  second: "/second-power?base=",
+  third: "/third-power?base=",
+};
