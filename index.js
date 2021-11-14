fed3b91160a59ec78b29b1a3f4082cd32676c677
NorbertRuff <ruffnorbert88@gmail.com>
2021-11-15 00:33:46 +0100

    [add] routes and business logic

--- index.js
+++ index.js
@@ -81,6 +81,76 @@ const fetchParallel = async (numbers) => {
   }
 };
 
+/**
+ * second-power route accepts an array returns API call results[]
+ * @returns {JSON<*[]>}
+ */
+app.get("/second-power", async function (request, response) {
+  let numbers = [];
+  try {
+    numbers = SplitRequestArray(request.query.values);
+  } catch (error) {
+    response.json("Wrong parameter syntax");
+  }
+  const poweredNumbers = await fetchSequentially(numbers);
+  response.json(poweredNumbers.toString());
+});
+
+/**
+ * third-power route accepts an array returns API call results[]
+ * @returns {JSON<*[]>}
+ */
+app.get("/third-power", async function (request, response) {
+  let numbers = [];
+  try {
+    numbers = SplitRequestArray(request.query.values);
+  } catch (error) {
+    response.json("Wrong parameter syntax");
+  }
+  let numbersChunks = chunkUpArray(numbers);
+  const poweredNumbers = [];
+  for (let numbersChunk of numbersChunks) {
+    poweredNumbers.push(await fetchParallel(numbersChunk));
+  }
+  response.json(poweredNumbers.toString());
+});
+
+/**
+ *  Sends Fetch req Sequentially
+ * @param numbers array of numbers
+ * @returns {Promise<*[]>}
+ */
+const fetchSequentially = async (numbers) => {
+  let poweredNumbers = [];
+  let data;
+  for (let number of numbers) {
+    try {
+      data = await fetchPow(API_BASE_URL + POWER_ROUTES.second + number);
+      poweredNumbers.push(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  return poweredNumbers;
+};
+/**
+ *
+ * @param numbers array of numbers
+ * @returns {Promise<String[]>}
+ */
+const fetchParallel = async (numbers) => {
+  try {
+    return await Promise.all(
+      numbers.map((number) =>
+        fetchPow(API_BASE_URL + POWER_ROUTES.third + number)
+      )
+    );
+  } catch (error) {
+    console.log(error.message);
+    throw error;
+  }
+};
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
